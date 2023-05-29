9b0ba07e71d384ada95ec2dc92d83fdde846757a
Julian Viso <julian.viso.romero@gmail.com>
2023-05-29 12:12:58 -0300

    form upgraded

--- src/pages/api/sendgrid.js
+++ src/pages/api/sendgrid.js
@@ -6,7 +6,7 @@ async function sendEmail(req, res) {
   try {
     // console.log("REQ.BODY", req.body);
     await sendgrid.send({
-      to: process.env.SENDGRID_EMAIL, // Your email where you'll receive emails
+      to: 'festivalestereorosario@gmail.com', // Your email where you'll receive emails
       from: process.env.SENDGRID_EMAIL, // your website email address here
       subject: `${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
