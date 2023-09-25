0375010e90cc2db7290f39a2eb7256f3d1025e0f
Julian Viso <julian.viso.romero@gmail.com>
2023-09-25 20:46:56 -0300

    inscribite

--- src/pages/lasraras.js
+++ src/pages/lasraras.js
@@ -49,7 +49,7 @@ export default function Festival() {
           </Container>
           <Container sx={styles.buttons}>
             <Box sx={{ backgroundColor: 'primary', maxWidth: ['100%', '80%'], mx: 'auto', mb: 2, fontSize: '1.5em', py: 1, textAlign: 'center', position: 'relative', fontFamily: 'Chakra Petch' }}>
-              <Link path={'https://mpago.la/2euD8ca'} target='_blank'>
+              <Link path={'https://forms.gle/gzb94NPafrQzsaGLA'} target='_blank'>
                 <Image src={parlante} sx={{ maxWidth: ['2em', '2.5em'], position: 'absolute', left: -2, top: [5, 3] }} />
                 <b>INSCRIBITE HACIENDO CLICK AQUÍ</b>
               </Link>
