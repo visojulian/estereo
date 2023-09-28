c37237a1e41a76fbb504d287d054c8712d365520
Julian Viso <julian.viso.romero@gmail.com>
2023-09-28 19:55:26 -0300

    /festival

--- src/pages/festival.js
+++ src/pages/festival.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'theme-ui';
 import { Box, Image, Text, Heading, Container } from 'theme-ui';
 import { StickyProvider } from '../contexts/app/app.provider';
@@ -14,8 +14,27 @@ import mate from 'assets/mate.png'
 import parlante from 'assets/parlante.png'
 import carlito from 'assets/carlito.png'
 import { Link } from 'components/link';
+import ModalImage, { Lightbox } from 'react-modal-image';
+import Landscape from 'assets/grilla-landscape.png'
+import Grilla from 'assets/grilla.png'
 
 export default function Festival() {
+  const [show, setShow] = useState(false);
+  const [viewportWidth, setViewportWidth] = useState(null);
+
+  useEffect(() => {
+    function handleResize() {
+      setViewportWidth(window.innerWidth);
+    }
+
+    setViewportWidth(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -35,11 +54,23 @@ export default function Festival() {
           </Container>
           <Container sx={styles.buttons}>
             <Image src={estrella} sx={{ maxWidth: '1.8em', mx: 'auto' }} />
-            <Box sx={{ backgroundColor: 'gray', maxWidth: ['100%', '80%'], mx: 'auto', mb: 2, fontSize: '1.5em', py: 1, textAlign: 'center', position: 'relative', fontFamily: 'Chakra Petch' }}>
-              <Link path={''}>
-                <Image src={parlante} sx={{ maxWidth: ['2em', '2.5em'], position: 'absolute', left: -2, top: [5, 3] }} />
-                <b>VER GRILLA</b>
-              </Link>
+            <Box
+              onClick={() => setShow(true)}
+              sx={{
+                backgroundColor: 'primary',
+                maxWidth: ['100%', '80%'],
+                mx: 'auto',
+                mb: 2,
+                fontSize: '1.5em',
+                py: 1,
+                textAlign: 'center',
+                position: 'relative',
+                fontFamily: 'Chakra Petch'
+              }}>
+
+              <Image src={parlante} sx={{ maxWidth: ['2em', '2.5em'], position: 'absolute', left: -2, top: [5, 3] }} />
+              <b>VER GRILLA</b>
+
             </Box>
             <Image src={estrella} sx={{ maxWidth: '1.8em', mx: 'auto' }} />
             <Box sx={{ backgroundColor: '#368DEC', maxWidth: ['100%', '80%'], mx: 'auto', mb: 2, fontSize: '1.5em', py: 1, textAlign: 'center', position: 'relative', fontFamily: 'Chakra Petch' }}>
@@ -49,8 +80,8 @@ export default function Festival() {
               </Link>
             </Box>
             <Image src={estrella} sx={{ maxWidth: '1.8em', mx: 'auto' }} />
-            <Box sx={{ backgroundColor: 'gray', maxWidth: ['100%', '80%'], mx: 'auto', mb: 2, fontSize: '1.5em', py: 1, textAlign: 'center', position: 'relative', fontFamily: 'Chakra Petch' }}>
-              <Link path={''}>
+            <Box sx={{ backgroundColor: 'third', maxWidth: ['100%', '80%'], mx: 'auto', mb: 2, fontSize: '1.5em', py: 1, textAlign: 'center', position: 'relative', fontFamily: 'Chakra Petch' }}>
+              <Link path={'/lasraras'}>
                 <Image src={carlito} sx={{ maxWidth: ['4em', '5em'], position: 'absolute', left: [1, 4, 4, 4, '8em'], bottom: -2 }} />
                 <b>TALLERES</b>
               </Link>
@@ -69,7 +100,7 @@ export default function Festival() {
               <Text as="p" variant="heroSecondary" sx={styles.text2}>
                 <b>Más de 20 presentaciones</b> en vivo en escenario, con sus protagonistas, para disfrutar las
                 narrativas más atractivas del momento. Acá te dejamos la grilla para que te reserves el
-                calendario. Recordá que todo es gratuito. <b><Link path={'#'} sx={{ color: 'gray' }}>Ver grilla (próximamente)</Link></b>.
+                calendario. Recordá que todo es gratuito. <b><a onClick={() => setShow(true)} style={{ cursor: 'pointer', color: '#368DEC' }}>Ver grilla</a></b>.
               </Text>
             </Box>
           </Container>
@@ -87,7 +118,7 @@ export default function Festival() {
               </Text>
               <Text as="p" variant="heroSecondary" sx={styles.text2}>
                 Y por último, este año desarrollamos nuestra primera <b>Maratón de Podcast</b>: un equipo de 20 personas coordinado por Jeremías Juárez va a crear un podcast de ficción en 48 horas que será estrenado en el cierre del Festival.<br />
-                Si te interesa acceder a cualquiera de estas actividades, lo podés hacer <b><Link path={'/convocatorias'}>desde acá</Link></b>.
+                Si te interesa acceder a cualquiera de estas actividades, lo podés hacer <b><Link style={{ color: '#368DEC' }} path={'/convocatorias'}>desde acá</Link></b>.
               </Text>
             </Box>
           </Container>
@@ -104,10 +135,18 @@ export default function Festival() {
                 Estéreo es un espacio de intercambio y desarrollo,de la industria del podcast. Además de todas las oportunidades para conectar que surgen en cada actividad del festival, creemos necesario formalizar algunos espacios de networking y conexión de productores, creativos y trabajadores de podcast como medio.<br />
                 Uno de estos espacios es el de <b>Showcase</b>, donde si tenés un proyecto de podcast que ya está en producción o todavía no pudiste iniciar y te gustaría tener feedback de productores profesionales, te invitamos a traerlo a nuestro festival. Vas a poder <b>contarle tu idea en 5 minutos a integrantes de las productoras más importantes de Argentina</b>, para recibir consejos y críticas sobre tu proyecto y para hacer importantes contactos para tu carrera profesional.<br />
                 Y, como siempre, habrá mesas de debate donde los productores más relevantes de la escena actual van a debatir sobre la actualidad y el futuro de nuestra industria, con espacio abierto a la participación del público.<br />
-                Si te interesa todo este mundo de conectividad y desarrollo, <b><Link path={'/convocatorias'}>acá te dejamos más info</Link></b>.
+                Si te interesa todo este mundo de conectividad y desarrollo, <b><Link style={{ color: '#368DEC' }} path={'/convocatorias'}>acá te dejamos más info</Link></b>.
               </Text>
             </Box>
             <hr />
+            {show &&
+              <Lightbox
+                hideDownload
+                hideZoom
+                large={viewportWidth > 1023 ? Landscape : Grilla}
+                onClose={() => setShow(false)}
+              />
+            }
           </Container>
         </Layout>
       </StickyProvider>
