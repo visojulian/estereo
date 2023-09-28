60d64f9896b84ac6f58608f6ab94b714d0fdd9f8
Julian Viso <julian.viso.romero@gmail.com>
2023-09-28 19:04:24 -0300

    grilla

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,18 +1,37 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import React, { useState, useEffect } from 'react';
 import { Container, Box, Heading, Text, Embed, Button } from 'theme-ui';
 import ShapeLeft from 'assets/shape-left.png';
 import Link from 'next/link';
 import ShapeRight from 'assets/shape-right.png';
+import Landscape from 'assets/grilla-landscape.png'
+import Grilla from 'assets/grilla.png'
+import Image from 'components/image';
 // const BannerVid = "https://www.youtube.com/embed/65rqGWf93AU";
 const BannerVid = "https://player.cloudinary.com/embed/?public_id=festiestereo&cloud_name=festivalestereo&player[seekThumbnails]=false&player[hideContextMenu]=true&player[floatingWhenNotVisible]=false&player[posterOptions][transformation][startOffset]=11";
 
 export default function Banner() {
+  const [viewportWidth, setViewportWidth] = useState(null);
+
+  useEffect(() => {
+    function handleResize() {
+      setViewportWidth(window.innerWidth);
+    }
+
+    setViewportWidth(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <section sx={styles.banner} id="home">
       <Container sx={styles.banner.container}>
         <Box sx={styles.banner.contentBox}>
-          <Box sx={styles.banner.text}>
+          {/* <Box sx={styles.banner.text}>
             <Heading as="h3">EDICIÓN 2023</Heading>
             <Heading as="h1" variant="heroPrimary">
               <span sx={{ letterSpacing: '10px' }}>6.7.8 </span>DE OCTUBRE
@@ -20,18 +39,49 @@ export default function Banner() {
             <Text as="p" variant="heroSecondary">
               Realizado en la ciudad de Rosario, Argentina <br /> Estéreo busca unir a creadores y oyentes en un mismo espacio. Brindando formaciones, conversatorios y shows gratuitos. Es el primer festival de podcast que se realiza en país.
             </Text>
-          </Box>
-          <Link
-            href={'/festival'}
-            spy={true}
-            smooth={true}
-          >
-            <Button variant="primary">
-              SABER MÁS
-            </Button>
-          </Link>
+          </Box> */}
+          {
+            viewportWidth > 1024 ?
+              <>
+                <Link
+                  href={'/festival'}
+                  spy={true}
+                  smooth={true}
+                >
+                  <Image src={Landscape} sx={{ mb: 4 }} />
+                </Link>
+                <Link
+                  href={'/festival'}
+                  spy={true}
+                  smooth={true}
+                >
+                  <Button variant="primary" sx={{ fontSize: '2em !important' }}>
+                    SABER MÁS
+                  </Button>
+                </Link>
+              </>
+              :
+              <>
+                <Link
+                  href={'/festival'}
+                  spy={true}
+                  smooth={true}
+                >
+                  <Image src={Grilla} sx={{ mb: 4 }} />
+                </Link>
+                <Link
+                  href={'/festival'}
+                  spy={true}
+                  smooth={true}
+                >
+                  <Button variant="primary" sx={{ fontSize: '1.5em !important' }}>
+                    SABER MÁS
+                  </Button>
+                </Link>
+              </>
+          }
         </Box>
-
+        {/*
         <Box sx={styles.banner.imageBox}>
           <Embed
             src={BannerVid}
@@ -40,7 +90,7 @@ export default function Banner() {
             allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
             frameBorder="0"
           />
-        </Box>
+        </Box> */}
       </Container>
     </section>
   );
@@ -48,7 +98,7 @@ export default function Banner() {
 
 const styles = {
   banner: {
-    pt: ['140px', '145px', '155px', '170px', null, null, '180px', '215px'],
+    pt: ['140px', '145px', '155px', '170px', null, null, '8em', '8em'],
     pb: [2, null, 0, null, 2, 0, null, 5],
     position: 'relative',
     zIndex: 2,
