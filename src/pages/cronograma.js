7e06ede25750a5211a8ab4894f2ca9f801614c5c
Julian Viso <julian.viso.romero@gmail.com>
2023-10-03 21:13:35 -0300

    cronorgrama

--- src/pages/cronograma.js
+++ src/pages/cronograma.js
@@ -6,6 +6,7 @@ import { StickyProvider } from 'contexts/app/app.provider'
 import React from 'react'
 import { ThemeProvider, Container, Box, Heading } from 'theme-ui'
 import theme from 'theme';
+import { Link } from 'components/link';
 
 export default function Cronograma() {
   return (
@@ -21,13 +22,21 @@ export default function Cronograma() {
               <table sx={{ ...styles.table, ...styles.viernes }}>
                 <thead>
                   <tr>
-                    <th colSpan={2}>Biblioteca Argentina</th>
+                    <th colSpan={2}>Biblioteca Argentina<br />
+                      <Link
+                        path={'https://maps.app.goo.gl/R3U7yXbGEeqmufX76'}
+                        target='_blank'
+                        sx={{ color: 'third' }}
+                      >
+                        Cómo llegar
+                      </Link>
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td>10:00 hs <br /> Periodismo especializado en Podcast</td>
-                    <td>¿Existe un periodismo especializado en podcast en Argentina? En esta charla, Pablo Fisher de Escucha Podcast, Malena Sánchez de Infobae y Morena Pardo de La Capital cuentan qué lugar ocupa el podcast en sus medios, cuál es el rol del periodismo en esta industria y cómo imaginan el futuro.</td>
+                    <td>10:00 hs <br /> Presentación del Circuito Iberoamericano de Festivales de Podcast.</td>
+                    <td>El Circuito Iberoamericano de Festivales de Podcast incluye eventos de España, Argentina, Colombia, Chile y Uruguay. En esta mesa, con presencia de representantes de Estéreo y Podcast Fest (Chile) más la participación por videollamada de los demás integrantes del Circuito, se conversarán los motivos de la creación de CIFESPOD y sus planes a futuro.</td>
                   </tr>
                   <tr>
                     <td>10:30 hs <br /> El podcast en la escuela</td>
@@ -42,31 +51,31 @@ export default function Cronograma() {
                     <td>Inauguración de la segunda edición de Estéreo: Festival argentino de Podcast.</td>
                   </tr>
                   <tr>
-                    <td>14:00 hs <br /> Presentación del Circuito Iberoamericano de Festivales de Podcast.</td>
-                    <td>El Circuito Iberoamericano de Festivales de Podcast incluye eventos de España, Argentina, Colombia, Chile y Uruguay. En esta mesa, con presencia de representantes de Estéreo y Podcast Fest (Chile) más la participación por videollamada de los demás integrantes del Circuito, se conversarán los motivos de la creación de CIFESPOD y sus planes a futuro.</td>
+                    <td>14:00 hs <br /> Periodismo especializado en Podcast</td>
+                    <td>¿Existe un periodismo especializado en podcast en Argentina? En esta charla, Pablo Fisher de Escucha Podcast, Malena Sánchez de Infobae y Morena Pardo de La Capital cuentan qué lugar ocupa el podcast en sus medios, cuál es el rol del periodismo en esta industria y cómo imaginan el futuro.</td>
                   </tr>
                   <tr>
-                    <td>14:30 hs <br /> El podcast en los medios</td>
-                    <td>¿Qué medios han incorporado al podcast a sus sistemas de producción y cómo lo hacen? En esta mesa, productores de medios nacionales discuten el lugar que tiene y puede tener el podcast en la radio y en los portales de noticias. ¿Qué estrategias se están utilizando? ¿Cómo conviven con la vieja guardia periodista? ¿Hay posibilidades de producir proyectos largos en medios tan acelerados?</td>
+                    <td>14:45 hs <br /> Lo que hacemos en las sombras (el rol de la producción)</td>
+                    <td>¿Qué hacen las y los productores? En esta mesa, cuatro profesionales multifacéticos deconstruyen la infinidad de detalles que conlleva la producción de un podcast, desde la conformación de los equipos de trabajo al armado de cronogramas, presupuestos y mil tareas invisibles más.</td>
                   </tr>
                   <tr>
-                    <td>15:00 hs <br /> El futuro del podcast de ficción en Latinoamérica</td>
+                    <td>15:15 hs <br /> El futuro del podcast de ficción en Latinoamérica</td>
                     <td>¿Tiene futuro el podcast de ficción en nuestro continente? En esta mesa, Maru Lombardo (Studio 80) e Ignacio Lara Saldaña (Emisor Podcasting) conversan acerca de los desafíos de producir podcasts de ficción. El trabajo con guionistas y actores, los costos de producción y marketing.</td>
                   </tr>
                   <tr>
-                    <td>15:30 hs <br /> ¿Cómo financiar un podcast?</td>
+                    <td>15:45 hs <br /> ¿Cómo financiar un podcast?</td>
                     <td>¿Cuáles son los caminos para conseguir el dinero necesario para producir un podcast? En esta mesa, referentes del podcast de Argentina y Chile, explican sus modelos de financiación y discuten sobre las dificultades y oportunidades que han encontrado.</td>
                   </tr>
                   <tr>
-                    <td>16:15 hs <br /> Lo que hacemos en las sombras (el rol de la producción)</td>
-                    <td>¿Qué hacen las y los productores? En esta mesa, cuatro profesionales multifacéticos deconstruyen la infinidad de detalles que conlleva la producción de un podcast, desde la conformación de los equipos de trabajo al armado de cronogramas, presupuestos y mil tareas invisibles más.</td>
+                    <td>16:15 hs <br /> El podcast en los medios</td>
+                    <td>¿Qué medios han incorporado al podcast a sus sistemas de producción y cómo lo hacen? En esta mesa, productores de medios nacionales discuten el lugar que tiene y puede tener el podcast en la radio y en los portales de noticias. ¿Qué estrategias se están utilizando? ¿Cómo conviven con la vieja guardia periodista? ¿Hay posibilidades de producir proyectos largos en medios tan acelerados?</td>
                   </tr>
                   <tr>
                     <td>17:00 hs <br /> Estreno: Quiero ser científica.</td>
                     <td>En este podcast en formato audioblog, cuatro chicas a punto de terminar la secundaria visitan laboratorios, charlan con científicos, aprenden y se divierten.</td>
                   </tr>
                   <tr>
-                    <td>17:30 hs <br /> Distribución y comercialización: lo menos sexy del podcast</td>
+                    <td>17:45 hs <br /> Distribución y comercialización: lo menos sexy del podcast</td>
                     <td>Ignacio Lara Saldaña, director de Emisor Podcasting (Chile) trae su clase magistral sobre el aspecto menos divertido, menos sexy y que nadie piensa a la hora de crear un podcast. Y es lo que pocos piensan al momento de decidir hacer un podcast.</td>
                   </tr>
                 </tbody>
@@ -76,7 +85,15 @@ export default function Cronograma() {
               <table sx={{ ...styles.table, ...styles.viernes }}>
                 <thead>
                   <tr>
-                    <th colSpan={2}>Galpón 11</th>
+                    <th colSpan={2}>Galpón 11<br />
+                      <Link
+                        path={'https://maps.app.goo.gl/LxZvPb4JQppgwHVq5'}
+                        target='_blank'
+                        sx={{ color: 'third' }}
+                      >
+                        Cómo llegar
+                      </Link>
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -124,20 +141,59 @@ export default function Cronograma() {
               <table sx={{ ...styles.table, ...styles.sabado }}>
                 <thead>
                   <tr>
-                    <th colSpan="2">Biblioteca Argentina</th>
+                    <th colSpan="2">Sede Gobierno UNR - Salón de los Espejos<br />
+                      <Link
+                        path={'https://maps.app.goo.gl/ZcwF5cwk2htzcJS68'}
+                        target='_blank'
+                        sx={{ color: 'third' }}
+                      >
+                        Cómo llegar
+                      </Link>
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <td >9:00 a 13:00 hs </td>
-                    <td> Taller: guión y audio para podcast documental: El paisaje sonoro de Las Raras. Partiendo desde la experiencia de Las Raras en la creación de distintos tipos de podcast documental, revisaremos en detalle las diferentes etapas de la producción y escucharemos ejemplos que ilustran cómo contar historias reales entendiendo al sonido como materia prima narrativa y estética. Única actividad arancelada. Más info aquí.</td>
+                    <td>9:00 a 13:00 hs </td>
+                    <td> Taller: guión y audio para podcast documental: El paisaje sonoro de Las Raras. Partiendo desde la experiencia de Las Raras en la creación de distintos tipos de podcast documental, revisaremos en detalle las diferentes etapas de la producción y escucharemos ejemplos que ilustran cómo contar historias reales entendiendo al sonido como materia prima narrativa y estética. Única actividad arancelada.</td>
                   </tr>
                 </tbody>
               </table>
               <table sx={{ ...styles.table, ...styles.sabado }}>
                 <thead>
                   <tr>
-                    <th colSpan="2">Galpón 11</th>
+                    <th colSpan="2">
+                      Sede UNR - Primer Piso Aula 1 <br />
+                      <Link
+                        path={'https://maps.app.goo.gl/iGwRjPosF114PdAi8'}
+                        target='_blank'
+                        sx={{ color: 'third' }}
+                      >
+                        Cómo llegar
+                      </Link>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>10:00 a 11:30 hs </td>
+                    <td> Taller: Escribe por los Derechos es una campaña global que Amnistía Internacional realiza todos los años durante el segundo semestre. En ella se invita a enviar cartas de apoyo a personas cuyos derechos humanos están siendo vulnerados en distintas partes del mundo y cartas para exigirles a las autoridades responsables de velar por esos derechos que actúen de manera acorde.
+                      Es una excelente propuesta para propiciar debates sobre derechos humanos teniendo en cuenta la situación a nivel global, a partir de casos de actualidad.</td>
+                  </tr>
+                </tbody>
+              </table>
+              <table sx={{ ...styles.table, ...styles.sabado }}>
+                <thead>
+                  <tr>
+                    <th colSpan="2">Galpón 11<br />
+                      <Link
+                        path={'https://maps.app.goo.gl/LxZvPb4JQppgwHVq5'}
+                        target='_blank'
+                        sx={{ color: 'third' }}
+                      >
+                        Cómo llegar
+                      </Link>
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -214,7 +270,15 @@ export default function Cronograma() {
               <table sx={{ ...styles.table, ...styles.domingo }}>
                 <thead>
                   <tr>
-                    <th colSpan={2}>Galpón 11</th>
+                    <th colSpan={2}>Galpón 11<br />
+                      <Link
+                        path={'https://maps.app.goo.gl/LxZvPb4JQppgwHVq5'}
+                        target='_blank'
+                        sx={{ color: 'third' }}
+                      >
+                        Cómo llegar
+                      </Link>
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -232,11 +296,11 @@ export default function Cronograma() {
                   </tr>
                   <tr>
                     <td>15:00 hs </td>
-                    <td> Creatividad en el branded podcast. ¿Cómo podemos ser creativos en los podcasts para empresas? En esta mesa, algunos de los líderes del mercado de branded podcast cuentan cómo hacen para inyectar creatividad y nuevas ideas en sus producciones. ¿Cómo establecer una relación de confianza con los clientes? ¿Cómo mantener el equilibrio entre lo artístico y lo funcional? ¿Cómo pitchear ideas?</td>
+                    <td> Trabajo freelance en podcast. La producción de podcast requiere de una gran variedad de saberes específicos, muchos de ellos contratados de manera freelance. En esta mesa, profesionales freelance dan un panorama sobre cómo es trabajar tanto para el podcast argentino como para productoras extranjeras, cómo organizar los tiempos y cómo ofrecer su trabajo de manera eficiente para ser contratados.</td>
                   </tr>
                   <tr>
                     <td>15:30 hs </td>
-                    <td> Trabajo freelance en podcast. La producción de podcast requiere de una gran variedad de saberes específicos, muchos de ellos contratados de manera freelance. En esta mesa, profesionales freelance dan un panorama sobre cómo es trabajar tanto para el podcast argentino como para productoras extranjeras, cómo organizar los tiempos y cómo ofrecer su trabajo de manera eficiente para ser contratados.</td>
+                    <td> Creatividad en el branded podcast. ¿Cómo podemos ser creativos en los podcasts para empresas? En esta mesa, algunos de los líderes del mercado de branded podcast cuentan cómo hacen para inyectar creatividad y nuevas ideas en sus producciones. ¿Cómo establecer una relación de confianza con los clientes? ¿Cómo mantener el equilibrio entre lo artístico y lo funcional? ¿Cómo pitchear ideas?</td>
                   </tr>
                 </tbody>
               </table>
@@ -244,7 +308,15 @@ export default function Cronograma() {
               <table sx={{ ...styles.table, ...styles.domingo }}>
                 <thead>
                   <tr>
-                    <th colSpan={2}>Cine El Cairo / Cierre festival</th>
+                    <th colSpan={2}>Cine El Cairo<br />
+                      <Link
+                        path={'https://maps.app.goo.gl/CxTNLzsM4uMz16WZ9'}
+                        target='_blank'
+                        sx={{ color: 'third' }}
+                      >
+                        Cómo llegar
+                      </Link>
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -278,8 +350,9 @@ const styles = {
     mb: 5,
     width: '100%',
     th: {
-      fontSize: '1.5em',
-      width: '100%'
+      fontSize: '1.2em',
+      width: '100%',
+      lineHeight: '1.2em'
     },
     td: {
       fontSize: '0.8em',
